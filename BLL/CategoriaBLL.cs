2940158e2daa2e374af76510678de549f078aa9b
maxg-dev <maximiliano.galvez@usm.cl>
2022-11-24 12:10:34 -0300

    primer commit

---
+++ BLL/CategoriaBLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    public class CategoriaBLL
+    {
+        ToDoListEntities toDoList = new ToDoListEntities();
+
+        public void Add(string nombre)
+        {
+            Categoria categoria = new Categoria();
+            categoria.Nombre = nombre;
+
+            toDoList.Categoria.Add(categoria);
+            toDoList.SaveChanges();
+        }
+
+        public List<Categoria> GetAll()
+        {
+            return toDoList.Categoria.ToList();
+        }
+    }
+}
---
+++ PL/Forms/UcAgregarCat.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BLL;
+
+namespace PL.Forms
+{
+    /// <summary>
+    /// Lógica de interacción para UcAgregarCat.xaml
+    /// </summary>
+    public partial class UcAgregarCat : UserControl
+    {
+        CategoriaBLL categoriabll = new CategoriaBLL();
+
+        public UcAgregarCat()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAgregarCat_Click(object sender, RoutedEventArgs e)
+        {
+            string nombre = txtNombreCat.Text.Trim();
+            categoriabll.Add(nombre);
+
+            MessageBox.Show("Categoría agregada :)", "Nueva Categoría", MessageBoxButton.OK, MessageBoxImage.Information);
+            txtNombreCat.Text = string.Empty;
+
+        }
+    }
+}
